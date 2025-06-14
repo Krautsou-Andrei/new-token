cf7d177ee79f747afe6b1921b56b3c719de21598
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2025-06-14 22:45:28 +0300

    feat: full screen if mobile

--- src/shared/ui/app-layout-bound/app-layout-bound.tsx
+++ src/shared/ui/app-layout-bound/app-layout-bound.tsx
@@ -1,19 +1,41 @@
-import { type ReactNode } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { type ReactNode, useEffect } from 'react';
 
 import { Box, type BoxProps } from '@chakra-ui/react';
-import { useInitData } from '@vkruglikov/react-telegram-web-app';
+import {
+  useExpand,
+  useInitData,
+  useWebApp,
+} from '@vkruglikov/react-telegram-web-app';
 
 import { useLogin } from '@/shared/api/hooks/auth/use-login';
+import { isMobileDevice } from '@/shared/lib/utils';
 
 interface AppLayoutBoundProps extends BoxProps {
   children: ReactNode;
 }
 
 export const AppLayoutBound = ({ children, ...props }: AppLayoutBoundProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const WebApp = useWebApp();
+
   const [_initDataUnsafe, initData] = useInitData();
+  const [_isExpanded, expand] = useExpand();
 
   useLogin(initData);
+  const isMobile = isMobileDevice();
+
+  useEffect(() => {
+    expand();
+    try {
+      if (isMobile) {
+        WebApp.requestFullscreen();
+        WebApp.disableVerticalSwipes();
+      }
+    } catch (err) {
+      /* empty */
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile]);
 
   return (
     <Box maxW="600px" mx="auto" px={5} {...props}>
